8623ec548d2299025c217687569f4bf53e04064b
FacuRiccardi <riccardifacu@gmail.com>
2023-06-21 12:04:53 -0300

    Users Model creation

---
+++ app/database/migrations/20230621140729-create-user.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.ENUM('ADMIN', 'VIEWER')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ app/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const { sequelize } = require('./models/index')
+
+//Settings
+const port = process.env.PORT || 3000;
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use(require("./routes"));
+
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`)
+
+    sequelize.authenticate().then(() => {
+        console.log('Connection to the Database successfull!')
+    })
+});
